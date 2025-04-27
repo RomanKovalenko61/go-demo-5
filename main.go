775ccbb024a5748284e508c3d0c018f8941f4fea
Roman <astek14@mail.ru>
2025-04-28 02:59:58 +0300

    15.3 CLI flags

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
 
 func main() {
 	fmt.Println("new project")
+	city := flag.String("city", "", "Город пользоввателя")
+	format := flag.Int("format", 1, "Формат вывода погоды")
+
+	flag.Parse()
+
+	fmt.Println(*city)
+	fmt.Println(*format)
 }

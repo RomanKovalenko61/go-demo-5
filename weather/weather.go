f47a177ee600845f844dc3e4fadd015358a4f640
Roman <astek14@mail.ru>
2025-04-28 06:04:21 +0300

    16.6 Exercise Errors

--- weather/weather.go
+++ weather/weather.go
@@ -2,31 +2,37 @@ package weather
 
 import (
 	"demo/weather/geo"
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
 	"net/url"
 )
 
-func GetWeather(geo geo.GeoData, format int) string {
+var ErrorNotValidURL = errors.New("ERROR_URL_IS_NOT_VALID")
+var ErrorNotResponce = errors.New("ERROR_NOT_RESPONCE")
+var ErrorCantReadResponce = errors.New("ERROR_CAN'T_READ_RESPONCE")
+var ErrorWrongFormat = errors.New("ERROR_WRONG_FORMAT")
+
+func GetWeather(geo geo.GeoData, format int) (string, error) {
+	if format < 1 || format > 4 {
+		return "", ErrorWrongFormat
+	}
 	baseUrl, err := url.Parse("https://wttr.in/" + geo.City)
 	if err != nil {
-		fmt.Println(err.Error())
-		return ""
+		return "", ErrorNotValidURL
 	}
 	params := url.Values{}
 	params.Add("format", fmt.Sprint(format))
 	baseUrl.RawQuery = params.Encode()
 	resp, err := http.Get(baseUrl.String())
 	if err != nil {
-		fmt.Println(err.Error())
-		return ""
+		return "", ErrorNotResponce
 	}
 	body, err := io.ReadAll(resp.Body)
 	defer resp.Body.Close()
 	if err != nil {
-		fmt.Println(err.Error())
-		return ""
+		return "", ErrorCantReadResponce
 	}
-	return string(body)
+	return string(body), nil
 }
--- weather/weather_test.go
+++ weather/weather_test.go
@@ -13,8 +13,23 @@ func TestGetWeather(t *testing.T) {
 		City: expected,
 	}
 	format := 3
-	result := weather.GetWeather(geoData, format)
+	result, err := weather.GetWeather(geoData, format)
+	if err != nil {
+		t.Errorf("Пришла ошибка %v", err)
+	}
 	if !strings.Contains(result, expected) {
 		t.Errorf("Ожидалось %v, получено %v", expected, result)
+	}
+}
+
+func TestGetWeatherWrongFormat(t *testing.T) {
+	expected := "Moskow"
+	geoData := geo.GeoData{
+		City: expected,
+	}
+	format := 125
+	_, err := weather.GetWeather(geoData, format)
+	if err != weather.ErrorWrongFormat {
+		t.Errorf("Ожидалось %v, получено %v", weather.ErrorWrongFormat, err)
 	}
 }

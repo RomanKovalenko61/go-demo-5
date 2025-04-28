c6a2c4d50cad7dd03e749043ff7226e02bca4901
Roman <astek14@mail.ru>
2025-04-28 04:43:47 +0300

    16.1 Arrange Act Assert

---
+++ geo/geo_test.go
@@ -0,0 +1,11 @@
+package geo_test
+
+import "testing"
+
+func TestGetMyLocation(t *testing.T) {
+	//Arange - подготовка, expected результат, данные для функции
+
+	//Act - выполняет функцию
+
+	//Assert - проверка результата с expected
+}

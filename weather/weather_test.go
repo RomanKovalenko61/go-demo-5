cbd9dd592372ebbc1515a3ddcb635ff74f93498d
Roman <astek14@mail.ru>
2025-04-28 05:31:29 +0300

    16.5 Exercise Test weather

---
+++ weather/weather_test.go
@@ -0,0 +1,20 @@
+package weather_test
+
+import (
+	"demo/weather/geo"
+	"demo/weather/weather"
+	"strings"
+	"testing"
+)
+
+func TestGetWeather(t *testing.T) {
+	expected := "Moskow"
+	geoData := geo.GeoData{
+		City: expected,
+	}
+	format := 3
+	result := weather.GetWeather(geoData, format)
+	if !strings.Contains(result, expected) {
+		t.Errorf("Ожидалось %v, получено %v", expected, result)
+	}
+}
